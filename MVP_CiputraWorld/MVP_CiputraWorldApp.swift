0e57b1fe54cb32e98882c90220c57ff4239dc346
nathangunawan <nathangun23@gmail.com>
2025-08-20 14:25:24 +0700

    Initial Commit

---
+++ MVP_CiputraWorld/MVP_CiputraWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVP_CiputraWorldApp.swift
+//  MVP_CiputraWorld
+//
+//  Created by Nathan Gunawan on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MVP_CiputraWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVP_CiputraWorldTests/MVP_CiputraWorldTests.swift
@@ -0,0 +1,16 @@
+//
+//  MVP_CiputraWorldTests.swift
+//  MVP_CiputraWorldTests
+//
+//  Created by Nathan Gunawan on 20/08/25.
+//
+
+import Testing
+
+struct MVP_CiputraWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
