a137355ac9bc5cd82de1f05c32724a0c411b46ee
keziaelice <127367044+keziaelice@users.noreply.github.com>
2025-08-28 15:13:03 +0700

    Add ImagePicker & HistoryView

---
+++ MVP_CiputraWorld/Features/HistoryCard/HistoryView.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryView.swift
+//  MVP_CiputraWorld
+//
+//  Created by Kezia Elice on 28/08/25.
+//
+
+import Foundation
---
+++ MVP_CiputraWorld/Features/HistoryCard/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  MVP_CiputraWorld
+//
+//  Created by Kezia Elice on 28/08/25.
+//
+
+import Foundation
