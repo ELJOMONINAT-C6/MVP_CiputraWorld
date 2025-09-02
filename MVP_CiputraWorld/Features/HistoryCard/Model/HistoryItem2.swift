6d33895373228f396c380e83b96a37100494bea8
keziaelice <127367044+keziaelice@users.noreply.github.com>
2025-09-02 14:09:36 +0700

    MVVM, add CameraView with timestamp

---
+++ MVP_CiputraWorld/Features/HistoryCard/Model/HistoryItem2.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryItem2.swift
+//  MVP_CiputraWorld
+//
+//  Created by Kezia Elice on 01/09/25.
+//
+
+import Foundation
+
+struct HistoryItem2: Identifiable {
+    let id = UUID()
+    let title: String
+    let date: Date
+}
